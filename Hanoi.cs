24b7df4f84c79b17df0faa62647d69521f3f6601
turalmamedov <105875974+turalmmv@users.noreply.github.com>
2022-10-21 03:40:42 +0400

    Add files via upload

---
+++ Hanoi.cs
@@ -0,0 +1,20 @@
+﻿using System;
+class GFG
+{
+    static void towerOfHanoi(int n, char from_rod,char to_rod, char helper_rod)
+    {
+        if (n == 0)
+        {
+            return;
+        }
+        towerOfHanoi(n - 1, from_rod, helper_rod, to_rod);
+        Console.WriteLine("Move disk " + n + " from rod " + from_rod + " to rod " + to_rod);
+        towerOfHanoi(n - 1, helper_rod, to_rod, from_rod);
+    }
+    public static void Main(String[] args)
+    {
+        int N = 3;
+
+        towerOfHanoi(N, 'A', 'C', 'B');
+    }
+}
