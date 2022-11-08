fa0292bba2ffe4a9b50e9940772d1ffb691f4bcf
turalmamedov <105875974+turalmmv@users.noreply.github.com>
2022-11-09 00:20:49 +0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0, b = 1, i, show = 0;
+            Console.WriteLine("Enter number: ");
+            string input = Console.ReadLine();
+            int n = int.Parse(input);
+            Console.WriteLine("Fibonacci numbers' count: " + n);
+
+            for (i = 0; i < n; i++)
+            {
+                show = a + b;
+                a = b;
+                b = show;
+                Console.WriteLine(show);
+            }
+
+        }
+    }
+}
